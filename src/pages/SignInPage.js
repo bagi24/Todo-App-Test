411a1401618f16d4e9ee7efcfd0a3d0793a10189
bagi24 <bagrat.injgia24@gmail.com>
2023-04-29 12:20:39 +0400

    signin design draft

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,17 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/SignInPage.css";
+import UploadPhoto from '../images/upload2.svg';
 
 export default function SignInPage() {
+
+const [file, setFile]= useState();
+
+console.log(file)
+
+const handleFile = (event) => {
+
+  setFile(event.target.files[0])
+
+  console.log(event.target.files[0])
+}
+
+
   return (
     <section className="register-content">
       <form>
         <h2> Get Started</h2>
 
-        <label className="input-labelPhoto" for="photo"> add a photo </label>
-        <input type="file" id="photo" name="photo" />
 
-        <label className="input-labelName" for="name"> fill in you name</label>
-        <input className="input-name" type="text" id="name"  name="name" placeholder="your name" />
+         <p> add a photo</p>
+        <label className="input-labelPhoto" for="photo">
+          <img className="uploadphoto" src={UploadPhoto} />
+        </label>
+        <input className="input-file" type="file" id="photo" name="photo" onChange={handleFile} />
+      
+        
+
+        <label className="input-labelName" for="name">
+          fill in you name
+        </label>
+        <input
+          className="input-name"
+          type="text"
+          id="name"
+          name="name"
+          placeholder="your name"
+        />
 
         <button className="btn-signin"> Sign In</button>
       </form>
