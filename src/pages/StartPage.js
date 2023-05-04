fc577490d5fad779cf94e4052a5e2ed29e93cb26
bagi24 <bagrat.injgia24@gmail.com>
2023-05-04 19:25:09 +0400

    redirect router

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from "../images/logo.png";
 import "../styles/StartPage.css";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 export default function StartPage() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("todos")) {
+      console.log("ehllo");
+      navigate("/todo-list");
+    }
+  }, []);
+
+  if(localStorage.getItem("todos")) return
+
   return (
     <div className="StartPage">
       <img className="todo-logo" src={logo} alt="logo" />
